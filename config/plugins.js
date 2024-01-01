4bd6968fd145fe019563c9face021312152a98fe
Vitaliy Starcev <6asm6deus6@gmail.com>
2024-01-01 16:09:37 +0300

    add: add vercel plugin

--- config/plugins.js
+++ config/plugins.js
@@ -5,11 +5,19 @@ module.exports = {
       responseTransforms: {
         removeAttributesKey: true,
         removeDataKey: true,
-      },      
+      },
       requestTransforms : {
         wrapBodyWithDataKey: true
       },
     },
   },
+  'vercel-deploy': {
+    enabled: true,
+    config: {
+      deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
+      apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
+      appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
+      roles: ["strapi-super-admin"],
+    },
+  },
 }
-
